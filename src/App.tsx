6524ccc14402fc181cbed54e42e98ac3fb3a75a5
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-01-31 23:49:16 -0300

    npm install react-router-dom - created home and history router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+
+import { Router } from './Router'
 
 export function App() {
   // envia variant como props
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
