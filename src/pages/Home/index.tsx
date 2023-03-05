e70cd9a89acd285bf1eb502ac61de6253f2f81a7
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-03-04 23:57:42 -0300

    add reset ao enviar o formulario

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,7 +26,7 @@ export function Home() {
   //register - funcao que retorna alguns metodos de input
   // handleSubmit - permite usar os dados do formulario
   // watch - observar algum elemento
-  const {register, handleSubmit, watch} = useForm<NewCycleFormData>({
+  const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -36,6 +36,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset() //apos enviar reseta os campos aos valores padrao
   }
 
 
