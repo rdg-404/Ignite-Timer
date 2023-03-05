dbad171a96b9a019db3fa790f87da4e8ed0e979b
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-03-04 23:36:31 -0300

    validacao dos campos do input com zod - hookform/resolver para integrar com react hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,34 @@
 import { Play } from "phosphor-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as zod from "zod" //importa tudo da lib com o nome de zod
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+
+//funcao de validacao dos campos
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+  .number()
+  .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+  .max(60, "O ciclo precisa ser de no máximo 60 minutos.")
+})
+
+
 
 export function Home() {
 
   //register - funcao que retorna alguns metodos de input
   // handleSubmit - permite usar os dados do formulario
   // watch - observar algum elemento
-  const {register, handleSubmit, watch} = useForm()
+  const {register, handleSubmit, watch} = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema)
+  })
+
+
+
+
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
