c211606f5e4be3bd20c44e3c30746f46449e64e1
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-03-18 23:30:12 -0300

    formatando a data usando funcoes do dateFNS e traduzindo para ptBR

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR";
 import { CycleContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
@@ -26,7 +28,10 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}</td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
