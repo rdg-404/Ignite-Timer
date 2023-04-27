221c2fe23c3438945ccc7014d90a15b64655fb85
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-04-26 22:07:31 -0300

    add immer ao interruptCycle e finishedCycle

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -26,30 +26,35 @@ export function cyclesReducer(state: CyclesState, action: any) {
         draft.cycles.push(action.payload.newCycle);
         draft.activeCycleId = action.payload.newCycle.id;
       })
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if(cycle.id === state.activeCycleId) {
-            return {...cycle, interruptedDate: new Date()}
-          }else {
-            return cycle
-          }
-        }),
-        activeCycleId: null
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:{
+
+      const currentCycleIndex = state.cycles.findIndex((cycle) =>{
+        return cycle.id === state.activeCycleId
+      })
+
+      if(currentCycleIndex < 0){
+        return state
       }
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if(cycle.id === state.activeCycleId) {
-            return {...cycle, finishedDate: new Date()}
-          }else {
-            return cycle
-          }
-        }),
-        activeCycleId: null
+
+      return produce(state, (draft) =>{
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].interruptedDate = new Date()
+      })
+    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:{
+      const currentCycleIndex = state.cycles.findIndex((cycle) =>{
+        return cycle.id === state.activeCycleId
+      })
+
+      if(currentCycleIndex < 0){
+        return state
       }
+
+      return produce(state, (draft) =>{
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
+      })
+    }
     default:
       return state
 
